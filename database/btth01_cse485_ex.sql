49ab826e1583474c26266a95ca1cf1f5d175bc9c
Pham Trong Nghia (Tristan) <phamnghia974@gmail.com>
2023-02-28 21:49:00 +0700

    add sql_ex and sql_data

--- database/btth01_cse485_ex.sql
+++ database/btth01_cse485_ex.sql
@@ -1,172 +1,136 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Feb 22, 2023 at 07:36 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.0.19
+-- Active: 1677135833192@@127.0.0.1@3306@btth01_cse485
+--a
+select baiviet.*, theloai.ten_tloai from baiviet, theloai
+where baiviet.ma_tloai = theloai.ma_tloai
+and theloai.ten_tloai like "Nhac tru tinh";
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+--b
+select baiviet.*, tacgia.ten_tgia from baiviet, tacgia
+where baiviet.ma_tgia = tacgia.ma_tgia
+and tacgia.ten_tgia like "Nhacvietplus";
+
+--c
+select theloai.* from theloai
+where theloai.ma_tloai not in (
+    select baiviet.ma_tloai
+    from baiviet
+);
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `btth01_cse485`
---
+--d
+select baiviet.ma_bviet, baiviet.tieude, baiviet.ten_bhat, tacgia.ten_tgia, theloai.ten_tloai, baiviet.ngayviet from baiviet, theloai, tacgia
+where baiviet.ma_tgia = tacgia.ma_tgia and baiviet.ma_tloai = theloai.ma_tloai;
 
--- --------------------------------------------------------
+--e
+select theloai.ten_tloai, count(*) as so_luong
+from theloai
+join baiviet
+on baiviet.ma_tloai = theloai.ma_tloai
+group by theloai.ma_tloai
+having count(baiviet.ma_tloai) = (
+select count(*) as so_luong
+from theloai
+left join baiviet
+on baiviet.ma_tloai = theloai.ma_tloai
+group by theloai.ten_tloai
+order by so_luong DESC
+limit 1
+);
 
---
--- Table structure for table `baiviet`
---
 
-CREATE TABLE `baiviet` (
-  `ma_bviet` int(10) UNSIGNED NOT NULL,
-  `tieude` varchar(200) NOT NULL,
-  `ten_bhat` varchar(100) NOT NULL,
-  `ma_tloai` int(10) UNSIGNED NOT NULL,
-  `tomtat` text NOT NULL,
-  `noidung` text DEFAULT NULL,
-  `ma_tgia` int(10) UNSIGNED NOT NULL,
-  `ngayviet` datetime NOT NULL DEFAULT current_timestamp(),
-  `hinhanh` varchar(200) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+--f
+select tacgia.ten_tgia, count(*) as so_luong
+from tacgia
+join baiviet
+where baiviet.ma_tgia = tacgia.ma_tgia
+group by tacgia.ma_tgia
+having count(baiviet.ma_tgia) = (
+select count(*) as so_luong
+from baiviet
+left join tacgia
+on baiviet.ma_tgia = tacgia.ma_tgia
+group by tacgia.ten_tgia
+order by so_luong desc
+limit 1
+);
 
---
--- Dumping data for table `baiviet`
---
+--g
+select baiviet.* from baiviet
+where baiviet.ten_bhat like "% yeu"
+or baiviet.ten_bhat like "% thuong"
+or baiviet.ten_bhat like "% anh" 
+or baiviet.ten_bhat like "% em";
 
-INSERT INTO `baiviet` (`ma_bviet`, `tieude`, `ten_bhat`, `ma_tloai`, `tomtat`, `noidung`, `ma_tgia`, `ngayviet`, `hinhanh`) VALUES
-(1, 'Lòng mẹ', 'Lòng mẹ', 2, 'Và mẹ ơi đừng khóc nhé! Cả đời này mẹ đã khóc nhiều lắm rồi, hãy cười lên vì con đã trưởng thành! Con sẽ lại về dậy sớm nấu cơm cho mẹ, nấu nước cho mẹ tắm như ngày xưa. “Dù cho vai nắng nhưng lòng thương chẳng nhạt màu, vẫn mơ quay về vui vầy dưới bóng mẹ yêu”', NULL, 1, '2012-07-23 00:00:00', NULL),
-(2, 'Cảm ơn em đã rời xa anh', 'Vết mưa', 2, 'Cảm ơn em đã cho anh những tháng ngày hạnh phúc, cho anh biết yêu và được yêu. Em cho anh được nếm trải hương vị ngọt ngào của tình yêu nhưng cũng đầy đau khổ và nước mắt. Những tháng ngày đó có lẽ suốt cuộc đời anh không bao giờ quên', NULL, 3, '2012-02-12 00:00:00', NULL),
-(3, 'Cuộc đời có mấy ngày mai?', 'Phôi pha', 2, 'Đêm nay, trời quang mây tạnh, trong người nghe hoang vắng và tôi ngồi đây “Ôm lòng đêm, Nhìn vầng trăng mới về” mà ngậm ngùi “Nhớ chân giang hồ. Ôi phù du, từng tuổi xuân đã già”', NULL, 4, '2014-03-13 00:00:00', NULL),
-(4, 'Quê tôi!', 'Quê hương', 5, 'Quê hương là gì mà chở đầy kí ức nhỏ xinh. Có đám trẻ nô đùa bên nhau dưới gốc ổi nhà bà Năm giữa trưa nắng gắt chỉ để chờ bà đi vắng là hái trộm. Có hai anh em tôi bì bõm lội sình bắt cua đem về nhà cho mẹ nấu canh, nấu cháo… Có ba chị em tôi lục đục tự nấu ăn khi mẹ vắng nhà. Có anh tôi luôn dắt tôi đi cùng đường ngõ xóm chỉ để em được vui. Có cả những trận cãi nhau nảy lửa của ba anh em nữa…', NULL, 5, '2014-02-20 00:00:00', NULL),
-(5, 'Đất nước', 'Đất nước', 5, 'Đã bao nhiêu lần tôi tự hỏi: liệu trên Thế giới này có nơi nào chiến tranh tang thương mà lại rất đổi anh hùng như nước mình không? Liệu có mảnh đất nào mà mỗi tấc đất hôm nay đã thấm máu xương của những thế hệ đi trước nhiều như nước mình không? Và, liệu có một đất nước nào lại có nhiều bà mẹ đau khổ nhưng cũng hết sức gan góc như đất nước mình không?', NULL, 1, '2010-05-25 00:00:00', NULL),
-(6, 'Hard Rock Hallelujah', 'Hard Rock Hallelujah', 7, 'Những linh hồn đang lạc lối, mù quáng mất phương hướng trong cõi trần gian đầy nghiệt ngã hãy nên lắng nghe \"Hard Rock Hallelujah\" để có thể quên tất cả mọi thứ để tìm về đúng bản chất sâu thẳm nhất trong tâm hồn chính mình!', NULL, 6, '2013-09-12 00:00:00', NULL),
-(7, 'The Unforgiven', 'The Unforgiven', 7, 'Lâu lắm rồi mới nghe lại The Unforgiven II, vì bài này không phải là bài mà tôi thích. Anh bạn tôi lúc trước, đi đâu cũng nghêu ngao bài này ấy, chỉ tại vì hắn đang... thất tình mà lị. Mà sao Metallica có The Unforgiven rồi lại có thêm bài này chi nữa vậy không biết nữa, làm cho tôi cảm thấy hình như hơi bị đúng so với tâm trạng của tôi lúc này.', NULL, 1, '2010-05-25 00:00:00', NULL),
-(8, 'Nơi tình yêu bắt đầu', 'Nơi tình yêu bắt đầu', 1, 'Nhiều người sẽ nghĩ làm gì có yêu nhất và làm gì có yêu mãi. Ừ! Chẳng có gì là mãi mãi cả, vì chúng ta không trường tồn vĩnh cửu', NULL, 1, '2014-02-03 00:00:00', NULL),
-(9, 'Love Me Like There’s No Tomorrow', 'Love Me Like There’s No Tomorrow', 8, 'Nếu ai đã từng yêu Queen, yêu cái chất giọng cao, sắc sảo như một vết cắt thật ngọt ẩn giấu bao cảm xúc mãnh liệt của Freddie chắc không thể không \"điêu đứng\" mỗi khi nghe Love Me Like There’s No Tomorrow.', NULL, 1, '2013-02-26 00:00:00', NULL),
-(10, 'I\'m stronger', 'I\'m stronger', 7, 'Em không phải là người giỏi giấu cảm xúc, nhưng em lại là người giỏi đoán biết cảm xúc của người khác vậy nên đừng cố nói nhớ em, rằng mọi thứ chỉ là do hoàn cảnh. Và cũng đừng dối em rằng anh đã từng yêu em. Em nhắm mắt cũng cảm nhận được mà, thật đấy', NULL, 2, '2013-08-21 00:00:00', NULL),
-(11, 'Ôi Cuộc Sống Mến Thương', 'Ôi Cuộc Sống Mến Thương', 5, 'Có một câu nói như thế này \"Âm nhạc là một cái gì khác lạ mà hầu như tôi muốn nói nó là một phép thần diệu.Vì nó đứng giữa tư tưởng và hiện tượng, tinh thần và vật chất, mọi thứ trung gian mơ hồ thế đó mà không là thế đó giữa các sự vật mà âm nhạc hòa giải\"', NULL, 2, '2011-10-09 00:00:00', NULL),
-(12, 'Cây và gió', 'Cây và gió', 7, 'Em và anh, hai đứa quen nhau thật tình cờ. Lời hát của anh từ bài hát “Cây và gió” đã làm tâm hồn em xao động. Nhưng sự thật phũ phàng rằng em chưa bao giờ nói cho anh biết những suy nghĩ tận sâu trong tim mình. Bởi vì em nhút nhát, em không dám đối mặt với thực tế khắc nghiệt, hay thực ra em không dám đối diện với chính mình.', NULL, 7, '2013-12-05 00:00:00', NULL),
-(13, 'Như một cách tạ ơn đời', 'Người thầy', 2, 'Ánh nắng cuối ngày rồi cũng sẽ tắt, dòng sông con đò rồi cũng sẽ rẽ sang một hướng khác. Nhưng việc trồng người luôn cảm thụ với chuyến đò ngang, cứ tần tảo đưa rồi lặng lẽ quay về đưa sang. Con đò năm xưa của Thầy nặng trĩu yêu thương, hy sinh thầm lặng.', NULL, 8, '2014-01-02 00:00:00', NULL);
+--h
+select baiviet.* from baiviet
+where (baiviet.ten_bhat like "% yeu"
+or baiviet.ten_bhat like "% thuong"
+or baiviet.ten_bhat like "% anh" 
+or baiviet.ten_bhat like "% em") OR
+(baiviet.tieude like "% yeu"
+or baiviet.tieude like "% thuong"
+or baiviet.tieude like "% anh"
+or baiviet.tieude like "% em"
+);
 
--- --------------------------------------------------------
+--i
+create view vw_Music as
+select baiviet.tieude, theloai.ten_tloai, tacgia.ten_tgia
+from baiviet 
+join theloai on baiviet.ma_tloai = theloai.ma_tloai 
+join tacgia on baiviet.ma_tgia = tacgia.ma_tgia;
 
---
--- Table structure for table `tacgia`
---
 
-CREATE TABLE `tacgia` (
-  `ma_tgia` int(10) UNSIGNED NOT NULL,
-  `ten_tgia` varchar(100) NOT NULL,
-  `hinh_tgia` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `tacgia`
---
+--j
+create procedure sp_DSBaiViet(
+    in ten_the_loai varchar(30)
+    )
+begin
+    declare khong_ton_tai condition for sqlstate '45000';
+    if EXISTS(select baiviet.* from baiviet, theloai 
+        where baiviet.ma_tloai = theloai.ma_tloai
+        and theloai.ten_tloai like ten_the_loai) = 0 
+        then
+        signal sqlstate '45000' set message_text = 'The loai khong ton tai';
+    else
+        (select baiviet.* from baiviet, theloai 
+        where baiviet.ma_tloai = theloai.ma_tloai
+        and theloai.ten_tloai like ten_the_loai);
+    end if;
+end;
 
-INSERT INTO `tacgia` (`ma_tgia`, `ten_tgia`, `hinh_tgia`) VALUES
-(1, 'Nhacvietplus', NULL),
-(2, 'Sưu tầm', NULL),
-(3, 'Sandy', NULL),
-(4, 'Lê Trung Ngân', NULL),
-(5, 'Khánh Ngọc', NULL),
-(6, 'Night Stalker', NULL),
-(7, 'Phạm Phương Anh', NULL),
-(8, 'Tâm tình', NULL);
+call sp_DSBaiViet("Nhac tre");
 
--- --------------------------------------------------------
+alter table theloai
+add column SLBaiViet int;
 
---
--- Table structure for table `theloai`
---
+update theloai
+set `SLBaiViet` = NULL;
+update theloai a, (select theloai.ma_tloai, count(*) cnt
+                    from theloai
+                    join baiviet
+                    on baiviet.ma_tloai = theloai.ma_tloai
+                    group by theloai.ma_tloai) b
+set a.`SLBaiViet` = b.cnt
+where a.ma_tloai = b.ma_tloai;
 
-CREATE TABLE `theloai` (
-  `ma_tloai` int(10) UNSIGNED NOT NULL,
-  `ten_tloai` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `theloai`
---
+--l
+create table Users (
+    id_taikhoan int not null AUTO_INCREMENT,
+    tai_khoan varchar(100),
+    mat_khau varchar(200),
+    primary key (id_taikhoan)
+);
 
-INSERT INTO `theloai` (`ma_tloai`, `ten_tloai`) VALUES
-(1, 'Nhạc trẻ'),
-(2, 'Nhạc trữ tình'),
-(3, 'Nhạc cách mạng'),
-(4, 'Nhạc thiếu nhi'),
-(5, 'Nhạc quê hương'),
-(6, 'POP'),
-(7, 'Rock'),
-(8, 'R&B');
+-- delete from baiviet where baiviet.ma_bviet = 15;
 
---
--- Indexes for dumped tables
---
+-- insert into baiviet  (ma_bviet, tieude, ten_bhat, ma_tloai, tomtat, ma_tgia, ngayviet)
+-- values(15, "Tình em", "Tình em", 2, "Em ơi em ở lại",1,now());
 
---
--- Indexes for table `baiviet`
---
-ALTER TABLE `baiviet`
-  ADD PRIMARY KEY (`ma_bviet`),
-  ADD KEY `ma_tloai` (`ma_tloai`),
-  ADD KEY `ma_tgia` (`ma_tgia`);
+-- update baiviet
+-- set ma_tloai = 4 where ma_bviet = 15;
 
---
--- Indexes for table `tacgia`
---
-ALTER TABLE `tacgia`
-  ADD PRIMARY KEY (`ma_tgia`);
 
---
--- Indexes for table `theloai`
---
-ALTER TABLE `theloai`
-  ADD PRIMARY KEY (`ma_tloai`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `baiviet`
---
-ALTER TABLE `baiviet`
-  MODIFY `ma_bviet` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
-
---
--- AUTO_INCREMENT for table `tacgia`
---
-ALTER TABLE `tacgia`
-  MODIFY `ma_tgia` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- AUTO_INCREMENT for table `theloai`
---
-ALTER TABLE `theloai`
-  MODIFY `ma_tloai` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `baiviet`
---
-ALTER TABLE `baiviet`
-  ADD CONSTRAINT `baiviet_ibfk_1` FOREIGN KEY (`ma_tloai`) REFERENCES `theloai` (`ma_tloai`),
-  ADD CONSTRAINT `baiviet_ibfk_2` FOREIGN KEY (`ma_tgia`) REFERENCES `tacgia` (`ma_tgia`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
