cf26aa268f98490336a95fadf0e5dd716b0411e5
Pham Trong Nghia (Tristan) <phamnghia974@gmail.com>
2023-03-04 18:12:10 +0700

    adding DBConnection and Article

--- model/Article.php
+++ model/Article.php
@@ -10,10 +10,37 @@ class Article
     
     private $theloai;
 
-    public function __construct()
+    public function __construct($tieude, $theloai, $tomtat, $noidung,  $tacgia)
     {
-
+        $this->tieude = $tieude;
+        $this->theloai = $theloai;
+        $this->tomtat = $tomtat;
+        $this->noidung = $noidung;
+        $this->tacgia = $tacgia;
     }
+
+    public function getTieuDe()
+    {
+        return $this->tieude;
+    }
+    public function getTheLoai()
+    {
+        return $this->theloai;
+    }
+    public function getTomTat()
+    {
+        return $this->tomtat;
+    }
+    public function getNoiDung()
+    {
+        return $this->noidung;
+    }
+
+    public function getTacGia()
+    {
+        return $this->tacgia;
+    }
+    
 }
 
 
